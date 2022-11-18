5c0d55ba0e6463e8393692de29e3984e0a003d69
Nikita Karpovich <obliviousnick@gmail.com>
2022-11-19 00:08:45 +0300

    entry script.php

--- script.php
+++ script.php
@@ -1,2 +1,16 @@
 <?php
-echo 'test';
+
+use Siren\CommissionTask\Controller;
+use Siren\CommissionTask\Input\CsvInput;
+
+require 'vendor/autoload.php';
+
+$filePath = $argv[1];
+try {
+    $input = new CsvInput($filePath);
+    $clientsOperations = $input->getInputData();
+    $controller = new Controller();
+    $controller->executeOperations($clientsOperations);
+} catch (Throwable $e) {
+    echo 'Error: ' . $e->getMessage();
+}
