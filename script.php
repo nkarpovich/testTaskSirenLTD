a637a8f98a9c24e608a8a8e8c77d3211e476047d
Nikita Karpovich <obliviousnick@gmail.com>
2022-11-20 23:19:23 +0300

    script.php small modifications

--- script.php
+++ script.php
@@ -5,10 +5,15 @@ use Siren\CommissionTask\Input\CsvInput;
 
 require 'vendor/autoload.php';
 
-$filePath = $argv[1];
 try {
+    //simplification of console input and input in general
+    $filePath = $argv[1];
+
+    //Get operations data
     $input = new CsvInput($filePath);
     $clientsOperations = $input->getInputData();
+
+    //Pass data to controller
     $controller = new Controller();
     $controller->executeOperations($clientsOperations);
 } catch (Throwable $e) {
